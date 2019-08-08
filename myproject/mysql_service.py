ea7ffd9a82d9e1c583b3746f3e199dbe234b3c99
RaviTeja.Poluri <raviteja.poluri1@gmail.com>
2019-08-08 23:24:38 +0530

    added ignore file and updated mysql_service file

--- myproject/mysql_service.py
+++ myproject/mysql_service.py
@@ -5,20 +5,19 @@ class MysqlDbConnSingleton:
     db_conn = None
 
     def create_db_conn(self):
-        if MysqlDbConnSingleton.db_conn is None:
-            MysqlDbConnSingleton.db_conn = mysql.connector.connect(
+        if self.db_conn is None:
+            self.db_conn = mysql.connector.connect(
                 host="localhost",
                 user="root",
                 passwd="root",
                 db="tourism",
                 auth_plugin="mysql_native_password"
             )
-        return MysqlDbConnSingleton.db_conn
 
     def __init__(self):
         self.create_db_conn()
 
-    def add_new_place(self, description="", name=None, nearest_airport="", nearest_bus_terminal="",
+    def add_new_place(self, description="", brief_info="", name=None, nearest_airport="", nearest_bus_terminal="",
                       nearest_railway_station="", state_name="", city_name=""):
         state_id = self.get_state_id_by_name(state_name)
         print("saving place %s for state %s of id %d" % (name, state_name, state_id))
@@ -31,9 +30,10 @@ class MysqlDbConnSingleton:
             last_id = 1
         else:
             last_id += 1
-        sql = "INSERT INTO places (id,description,name,nearest_airport,nearest_bus_terminal,nearest_railway_station," \
-              "state_id,village_or_city) VALUES (%s,%s, %s,%s,%s,%s,%s,%s)"
-        val = (last_id, description, name, nearest_airport, nearest_bus_terminal,
+        sql = "INSERT INTO places (id,description,brief_info,name,nearest_airport,nearest_bus_terminal," \
+              "nearest_railway_station," \
+              "state_id,village_or_city) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        val = (last_id, description, brief_info, name, nearest_airport, nearest_bus_terminal,
                nearest_railway_station, str(state_id), city_name)
         print("executing query '", sql % val, "'")
         my_cursor.execute(sql, val)
